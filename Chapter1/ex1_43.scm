7e052e5202e84ea08021625fb86c5fd83740205e
Renkai <gaelookair@gmail.com>
2013-11-16 16:25:37 +0800

    加注释

--- Chapter1/ex1_43.scm
+++ Chapter1/ex1_43.scm
@@ -1,6 +1,6 @@
 (define (repeated f n)
   (if (= n 0)
-      (lambda (x) x)
+      (lambda (x) x)			;运算过程其实是括号一层层剥掉的过程,(lambda (x) (x))每次运算都不会减括号数量,所以会永无止境
       (lambda (x)
 	(f ((repeated f (- n 1)) x)))))
 
